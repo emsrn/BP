a160624db670d7903a39d6cfab23d08ecddbe158
emsrn <sabourin.emma2@gmail.com>
2026-03-24 11:21:23 +0100

    Update custom.ts

--- custom.ts
+++ custom.ts
@@ -2,15 +2,37 @@ enum Choice {
     C,
     D
 }
+
 //% weight=100 color=#ff69b4 icon=""
 namespace tapis {
 
     //% block="Bouton %choice ON/OFF"
-    export function bouton (choice: Choice){
+    export function bouton(choice: Choice): void {
+
         if (choice == Choice.C) {
-            pins.digitalWritePin(DigitalPin.P14, 1)
-        } else if (choice == Choice.D) {
-            pins.digitalWritePin(DigitalPin.P13, 1)
+            // si bouton C choisi 
+            if (pins.digitalReadPin(DigitalPin.P14) == 1) {
+                // moteur ON 
+                pins.digitalWritePin(DigitalPin.P15, 1)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            } else {
+                // moteur OFF
+                pins.digitalWritePin(DigitalPin.P15, 0)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            }
+        }
+
+        if (choice == Choice.D) {
+            // si bouton D choisi 
+            if (pins.digitalReadPin(DigitalPin.P13) == 1) {
+                // moteur ON 
+                pins.digitalWritePin(DigitalPin.P15, 1)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            } else {
+                // moteur OFF
+                pins.digitalWritePin(DigitalPin.P15, 0)
+                pins.digitalWritePin(DigitalPin.P16, 0)
+            }
         }
 
     }
